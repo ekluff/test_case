7f1bc0cdcd29c619f4272931b60b1ae1a7265e8f
Evan Clough <clough.evan@gmail.com>
2015-08-31 10:46:01 -0700

    Remove code to handle 'special case' words. Code was never implemented.

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,20 +1,17 @@
 class String
   prepositions = ["a", "and", "apud", "the", "this"]
-  # special = ["mcduff"]
 
   define_method(:title_case) do
     split_sentence = self.split()
     split_sentence.each() do |word|
-      # unless special.include?(word.downcase())
       word.capitalize!()
       if prepositions.include?(word.downcase())
         word.downcase!()
       end
-      # end
 
     end
 
-    split_sentence[0].capitalize!() #Capitalizes first letter and lowercases rest of first word in sentence. Modifies special cases undesirably. Could not figure out how to leave special cases alone while capitalizing all other first words.
+    split_sentence[0].capitalize!()
     split_sentence.join(" ")
 
   end
